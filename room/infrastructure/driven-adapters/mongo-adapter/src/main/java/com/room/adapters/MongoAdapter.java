4fa1d5860c6d3a69bc03a005e425eb62337ac8bc
Leandro Hernandez <l.hernandez071999@gmail.com>
2024-12-16 23:54:38 -0300

    Add: Avances

--- room/infrastructure/driven-adapters/mongo-adapter/src/main/java/com/room/adapters/MongoAdapter.java
+++ room/infrastructure/driven-adapters/mongo-adapter/src/main/java/com/room/adapters/MongoAdapter.java
@@ -1,15 +1,26 @@
 package com.room.adapters;
 
+import com.room.Room;
+import com.room.out.RoomRepository;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public class MongoAdapter{
+public class MongoAdapter implements RoomRepository{
 
     private final MongoTemplate mongoTemplate;
 
     public MongoAdapter(MongoTemplate mongoTemplate) {
         this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public void save(Room room) {
+        mongoTemplate.save(room);
+    }
+
+    @Override
+    public Room findRoomByName(Room room) {
+        return null;
     }
 }
---
+++ room/infrastructure/driven-adapters/mongo-adapter/src/main/java/com/room/entities/RoomEntity.java
@@ -0,0 +1,9 @@
+package com.room.entities;
+
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Room")
+public class RoomEntity {
+
+}
